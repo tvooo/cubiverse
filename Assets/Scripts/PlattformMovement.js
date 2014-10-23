5224f055bb6ba4fd74ba426d1b0915410e4672ad
Grapemind <sonja.rattay@gmail.com>
2014-10-23 12:38:13 +0200

    Added Platform + Rotator scripts

---
+++ Assets/Scripts/PlattformMovement.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var lala : float = 0.05;
+var col : boolean = false;
+
+function Start () {
+
+}
+
+
+function Update () {
+	transform.Translate(-lala, 0, 0);
+}
+
+function OnTriggerEnter(collider : Collider) {
+	Debug.Log("I am colliding");
+	//transform.Translate(lala, 0, 0);
+	lala = -lala;
+	//Debug.Log("I am colliding");
+}
