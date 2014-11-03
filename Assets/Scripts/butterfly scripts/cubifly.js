c185f1fdc95618e25e0b0726b5dfd900208cccd3
Tim von Oldenburg <tim@tvooo.de>
2014-11-03 20:45:06 +0100

    Sligth cubifly variation

--- Assets/Scripts/butterfly scripts/cubifly.js
+++ Assets/Scripts/butterfly scripts/cubifly.js
@@ -1,11 +1,13 @@
 ﻿#pragma strict
 
+public var offset: float = 0f;
+
 function Start () {
 
 }
 
 function Update () {
-	transform.Rotate (new Vector3 (0, -90, 0) * Time.deltaTime);
+	transform.Rotate (new Vector3 (0, -90, 0) * (Time.deltaTime + offset));
 }
 
 
